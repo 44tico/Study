24beec5b781522721ed018389664daa737047380
44tico <joaoguilhermemgusmao@gmail.com>
2025-05-29 21:58:17 -0300

    base

---
+++ app/src/main/java/com/joao/study/views/SignInScreen.kt
@@ -0,0 +1,31 @@
+package com.joao.study.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.joao.study.R
+
+@Composable
+fun SignInScreen(){
+}
+
+@Preview
+@Composable
+fun SignInScreenPreview(){
+    Box(Modifier.fillMaxSize().background(Color.White)) {
+        SignInScreen()
+    }
+}
