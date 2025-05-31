fa1316f7541bc03e7c58d37928e5befbbbc3e6b1
44tico <joaoguilhermemgusmao@gmail.com>
2025-05-31 12:37:29 -0300

    chore: organiza nome dos icones no codigo

--- app/src/main/java/com/joao/study/views/SignInScreen.kt
+++ app/src/main/java/com/joao/study/views/SignInScreen.kt
@@ -1,33 +1,29 @@
 package com.joao.study.views
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,12 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -98,7 +89,7 @@ fun SignInScreen(){
     var password by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize().background(Background),
+        modifier = Modifier.fillMaxSize().background(Background).padding(WindowInsets.safeDrawing.asPaddingValues()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -203,7 +194,7 @@ fun SignInScreen(){
             ) {
                 IconButton(onClick = { TODO() }) {
                     Icon(
-                        painter = painterResource(id = R.drawable.google),
+                        painter = painterResource(id = R.drawable.google_icon),
                         contentDescription = null,
                         tint = Color.Unspecified,
                         modifier = Modifier.size(20.dp)
@@ -222,7 +213,7 @@ fun SignInScreen(){
             ) {
                 IconButton(onClick = { TODO() }) {
                     Icon(
-                        painter = painterResource(id = R.drawable.phone),
+                        painter = painterResource(id = R.drawable.phone_icon),
                         contentDescription = null,
                         tint = Color.Unspecified,
                         modifier = Modifier.size(25.dp)
