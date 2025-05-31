45b6ae8896e912cdc0cec7167888dafa9a626a94
44tico <joaoguilhermemgusmao@gmail.com>
2025-05-31 12:25:06 -0300

    style: organiza arquivo de cores

--- app/src/main/java/com/joao/study/ui/theme/Color.kt
+++ app/src/main/java/com/joao/study/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.joao.study.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Dark
 val Background = Color(0xfff121212)
 val BackgroundButton = Color(0Xff1A1A1A)
 val White50 = Color(0x80ffffff)
 val White30 = Color(0x4Dffffff)
 val White20 = Color(0x33ffffff)
-
 val Gold = Color(0xffB29146)
+
+// Light
